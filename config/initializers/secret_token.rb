12b539c72a4650b3ca8095e01320e64ba4c18963
Breisa Moralde <breisa.moralde@orangeandbronze.com>
2013-08-25 07:13:21 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Stammr::Application.config.secret_key_base = '8c447f987ea388d3da99cca2e0484ba12a2eef4a53627d8f2ad87eed3125c4edc678aebc29e7ab4162a2dbdc71516532fdfbbe966038ab6d8e8d6654ecca0582'
