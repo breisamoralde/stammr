245d0bbb5e0b49d11cd1454d242eae5fcf4e4f84
Breisa Moralde <breisa.moralde@orangeandbronze.com>
2013-08-25 21:41:39 +0800

    added unique:true to stammrPost

--- app/models/stammr_post.rb
+++ app/models/stammr_post.rb
@@ -1,4 +1,4 @@
 class StammrPost < ActiveRecord::Base
   has_many :comments,  :dependent => :destroy
-  validates :content,  presence: true
+  validates :content,  presence: true, uniqueness: true
 end
